4d0ddf2b4179a5d6c6c013ca2d2a16444ad59d4d
Claire Bain <claire.bain3@gmail.com>
2022-10-31 21:48:26 -0700

    fixes CSS layout

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 var currentDayEl = document.getElementById('currentDay');
 var calendarEl = document.getElementById('calendar');
 var spanTags = document.querySelectorAll("span");
-
+// declare variables
 var currentDate = moment().format('dddd MMMM Do');
 var currentHour = moment().format('HH')
-console.log(currentHour)
+let alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split(
+    ""
+  );
+// display current date on webpage
 currentDayEl.textContent = currentDate
 // set the class for each span tag based on the current time
+// add any data from local storage
 for (var i = 0; i < spanTags.length; i++) {
     if (i + 9 > currentHour){
     spanTags[i].setAttribute("class", "future");
@@ -26,35 +30,30 @@ for (var i = 0; i < spanTags.length; i++) {
         spanTags[i].textContent = localStorage.getItem(spanTags[i].id)
     }
 }
-
+// event listener for calendar
 calendarEl.addEventListener("click", function (event) {
     var element = event.target;
+    // check if the click was on a calendar hour
     if (element.matches(".future") || (element.matches(".past")) || (element.matches(".present"))) {
-        // element.setAttribute("class", "textarea")
-        console.log("ok")
         // EVENT LISTENER: type a key
         // Keydown event
         document.addEventListener("keydown", function (event) {
-            console.log("keydown happened");
+           // check if key is alphanumeric
             var key = event.key;
+            if (alphabetNumericCharacters.includes(key)) {
                 element.textContent += event.key;
-              
-            
-          });
+    }});
 
     }
-    else( console.log("not ok"))
 });
-
+// event listener to save the text in that row to local storage when save button is clicked
 calendarEl.addEventListener("click", function(event){
 var element = event.target;
 if (element.matches(".saveBtn")){
     var rowParent = (element.closest("div"))
-    console.log(rowParent)
     var currentSpan = rowParent.querySelector("span")
     // var textContent = rowParent.children('span').eq(0).textContent
     var currentSpanId = currentSpan.id
-    console.log(currentSpanId)
     localStorage.setItem(currentSpanId, currentSpan.textContent);
 }
 
